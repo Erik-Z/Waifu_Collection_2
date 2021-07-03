0107b48d8884f1314c10328255c206ce9470ff9a
Erik Z <erikzhou7@gmail.com>
2021-07-03 18:28:45 -0400

    Turned Model Into a component

--- views/HomeContent.js
+++ views/HomeContent.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Modal, TouchableHighlight, Alert} from 'react-native';
-import { Appbar, Divider } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 import Constants from "expo-constants"
 import axios from 'axios';
 import WaifuList from '../components/WaifuList';
+import CustomModal from '../components/Modal';
 const HomeContent = ({navigation, userData}) => {
     const [waifus, setWaifus] = useState([])
     const [modalVisible, setModalVisible] = useState(false);
@@ -30,25 +31,14 @@ const HomeContent = ({navigation, userData}) => {
                 <Appbar.Action icon="information" onPress={() => {setModalVisible(true)}}/>
             </Appbar>
             <WaifuList waifus={waifus} navigation={navigation}/>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}>
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Welcome to Waifu Collection!</Text>
-                        <Text style={styles.modalText}>This is the home page. To upload an image, hit the menu button next to 'Home' and goto the 'Create Waifu' tab. </Text>
-                        <Text style={styles.modalText}>If you want to see what other people posted, goto the 'All Waifus' tab</Text>
-                        <TouchableHighlight
-                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-                        onPress={() => {
-                            setModalVisible(!modalVisible);
-                        }}>
-                        <Text style={styles.textStyle}>Close</Text>
-                        </TouchableHighlight>
-                    </View>
-                </View>
-            </Modal>
+            <CustomModal 
+            texts={[
+                "Welcome to Waifu Collection",
+                "This is the home page. To upload an image, hit the menu button next to 'Home' and goto the 'Create Waifu' tab.",
+                "If you want to ssee what other people posted, goto the 'All Waifus' tab"
+            ]}
+            visibleState={modalVisible}
+            setVisibleState={setModalVisible}/>        
         </View>
     )
 }
@@ -65,42 +55,6 @@ const styles = StyleSheet.create({
     },
     container:{
         flex: 1
-    },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
-    },
-    modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    openButton: {
-        backgroundColor: '#F194FF',
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-      },
-    textStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: 'center',
     },
 })
 
