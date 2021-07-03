7c288f581cc37eaeae136a980db5d69cb81fde7e
Erik Z <erikzhou7@gmail.com>
2021-07-03 17:49:34 -0400

    Added Home Screen Information Model

--- views/HomeContent.js
+++ views/HomeContent.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { StyleSheet, Text, View, Modal, TouchableHighlight, Alert} from 'react-native';
+import { Appbar, Divider } from 'react-native-paper';
 import Constants from "expo-constants"
 import axios from 'axios';
 import WaifuList from '../components/WaifuList';
 const HomeContent = ({navigation, userData}) => {
     const [waifus, setWaifus] = useState([])
+    const [modalVisible, setModalVisible] = useState(false);
     useEffect(() => {
         if(userData){
             getWaifus()
@@ -25,9 +26,29 @@ const HomeContent = ({navigation, userData}) => {
         <View style={{flex: 1}}>
             <Appbar style={styles.appbar}>
                 <Appbar.Action icon="menu" onPress={() => navigation.toggleDrawer()} />
-                <Text style={styles.appHeader}> Home </Text>
+                <Appbar.Content titleStyle={styles.appHeader} title="Home"/>
+                <Appbar.Action icon="information" onPress={() => {setModalVisible(true)}}/>
             </Appbar>
             <WaifuList waifus={waifus} navigation={navigation}/>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Welcome to Waifu Collection!</Text>
+                        <Text style={styles.modalText}>This is the home page. To upload an image, hit the menu button next to 'Home' and goto the 'Create Waifu' tab. </Text>
+                        <Text style={styles.modalText}>If you want to see what other people posted, goto the 'All Waifus' tab</Text>
+                        <TouchableHighlight
+                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                        }}>
+                        <Text style={styles.textStyle}>Close</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -39,11 +60,48 @@ const styles = StyleSheet.create({
     appHeader:{
         fontSize: 20,
         fontWeight: "bold",
-        color: "#FFFFFF"
+        color: "#FFFFFF",
+        marginLeft: -15
     },
     container:{
         flex: 1
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    openButton: {
+        backgroundColor: '#F194FF',
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
 })
 
 export default HomeContent
