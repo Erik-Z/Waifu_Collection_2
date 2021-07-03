21f32390bf08fa24c1b2eb7eb3efa110c701bbd6
Erik Z <erikzhou7@gmail.com>
2021-07-02 21:18:20 -0400

    Can now download image from waifu details

--- views/WaifuDetails.js
+++ views/WaifuDetails.js
@@ -1,17 +1,59 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView} from 'react-native';
-import { Appbar, Card, Title, Paragraph, IconButton, Colors, Divider, Subheading, Caption } from 'react-native-paper';
+import { Appbar, Card, Title, Paragraph, IconButton, Provider, Menu, Button,
+        Colors, Divider, Subheading, Caption, Snackbar } from 'react-native-paper';
 import Constants from "expo-constants"
+import * as MediaLibrary from "expo-media-library"
+import * as FileSystem from "expo-file-system"
+
 const WaifuDetails = ({navigation, userData, route}) => {
+    const [visible, setVisible] = useState(false)
+    const [snackbarVisible, setSnackbarVisible] = useState(false)
+    const openMenu = () => setVisible(true)
+    const closeMenu = () => setVisible(false)
+    const openSnackbar = () => setSnackbarVisible(true)
+    const closeSnackbar = () => setSnackbarVisible(false)
+    const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical'
+    const downloadImage = async () => {
+        const { status } = await MediaLibrary.requestPermissionsAsync()
+        
+        if (status !== "granted") {
+            alert('Gallary Access needed to Upload Image');
+        } else {
+            //store the cached file
+            const file = await FileSystem.downloadAsync(
+                route.params.waifu.image,
+                FileSystem.documentDirectory + route.params.waifu.name +".jpg"
+            );
+            
+            //save the image in the galery using the link of the cached file
+            // const assetLink = await MediaLibrary.createAssetAsync(file.uri);
+            // console.log(file, assetLink);
+            await MediaLibrary.createAssetAsync(file.uri)
+            .then(()=>{
+                closeMenu()
+                openSnackbar()
+            })
+        }
+    }
     return (
+        <Provider>
         <View style={{flex: 1}}>
+            <Menu
+                visible={visible}
+                onDismiss={closeMenu}
+                anchor={{ x: Dimensions.get('window').width, y: 70 }}>
+                <Menu.Item onPress={downloadImage} title="Download Image" />
+                <Menu.Item onPress={() => {}} title="Report" />
+            </Menu>
             <Appbar style={styles.appbar}>
                 <Appbar.Action icon="keyboard-backspace" onPress={() => navigation.goBack()} />
-                <Text style={styles.appHeader}> {route.params.waifu.name} </Text>
+                <Appbar.Content titleStyle={styles.appHeader} title={route.params.waifu.name}/>
+                <Appbar.Action icon={MORE_ICON} onPress={openMenu} />
             </Appbar>
             <SafeAreaView style={{flex: 1}}>
                 <ScrollView>
-                    <Card>
+                    <Card style={{marginTop: 5}}>
                         <Card.Cover style={styles.image} source={{ uri: route.params.waifu.image }} />
                         <Card.Content>
                             <Title>
@@ -46,7 +88,19 @@ const WaifuDetails = ({navigation, userData, route}) => {
                     </Card>
                 </ScrollView>
             </SafeAreaView>
+            <Snackbar
+                visible={snackbarVisible}
+                onDismiss={closeSnackbar}
+                action={{
+                label: 'Dismiss',
+                onPress: () => {
+                    closeSnackbar()
+                },
+                }}>
+                Image Successfully Downloaded
+            </Snackbar>
         </View> 
+        </Provider>
     )
 }
 const styles = StyleSheet.create({
@@ -60,12 +114,8 @@ const styles = StyleSheet.create({
     appHeader:{
         fontSize: 20,
         fontWeight: "bold",
-        color: "#FFFFFF"
+        color: "#FFFFFF",
+        marginLeft: -15
     },
-    detailedText:{
-        fontSize: 20,
-        fontWeight: "bold",
-
-    }
 })
 export default WaifuDetails
