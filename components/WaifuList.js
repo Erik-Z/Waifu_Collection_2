e6c14620a8e8636890c652b6b942b43b256829f5
Erik Z <erikzhou7@gmail.com>
2021-07-19 11:39:00 -0400

    Implemented Waifu Filtering.

--- components/WaifuList.js
+++ components/WaifuList.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { SafeAreaView, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import { useFocusEffect } from '@react-navigation/native';
 import WaifuPreview from '../components/WaifuPreview';
+import { Filters } from '../views/HomeContent';
+import axios from 'axios';
 const WaifuList = (props) => {
-    const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState('')
+    const [userInfo, setUserInfo] = useState(null)
+    useFocusEffect(
+        useCallback(() => {
+            console.log("WaifuList Mounted")
+            axios.get("http://192.168.1.199:3000/get-user", {
+                params: {username: props.userData.username}
+            })
+            .then(res => {
+                if (!res.data){
+                    console.log("Not suppose to show")
+                } else {
+                    setUserInfo(res.data)
+                }
+            })
+            .catch(err => console.log(err))
+
+            return () => {
+                console.log("Waifu List UnMounted")
+            }
+        }, [])
+    )
     const renderWaifuCard = ({ item }) => {
         return <WaifuPreview uri={item.image} name={item.name} series={item.series}
                     gotoDetails={() => {
@@ -11,6 +35,43 @@ const WaifuList = (props) => {
                     }}
                 />
     }
+    
+    const likedFilter = (waifuList) => {
+        return waifuList.filter(waifu => {return userInfo.likedWaifus.includes(waifu._id)})
+    }
+
+    const followedFilter = (waifuList) => {
+        return waifuList.filter(waifu => {return userInfo.followersList.includes(waifu.owner)})
+    }
+
+    const filterWaifuList = () => {
+        if (props.filter === Filters.MY_WAIFUS){
+            return (
+                <FlatList
+                    data={props.waifus.filter(waifu => { return waifu.name.toLowerCase().includes(searchQuery.toLowerCase())})}
+                    renderItem={renderWaifuCard}
+                    keyExtractor={item => item._id}
+                />
+            )
+        } else if (props.filter === Filters.LIKED){
+            return (
+                <FlatList
+                    data={likedFilter(props.waifus.filter(waifu => { return waifu.name.toLowerCase().includes(searchQuery.toLowerCase())}))}
+                    renderItem={renderWaifuCard}
+                    keyExtractor={item => item._id}
+                />
+            )
+        } else if (props.filter === Filters.FOLLOWED){
+            return (
+                <FlatList
+                    data={followedFilter(props.waifus.filter(waifu => { return waifu.name.toLowerCase().includes(searchQuery.toLowerCase())}))}
+                    renderItem={renderWaifuCard}
+                    keyExtractor={item => item._id}
+                />
+            )
+        }
+    }
+
     return (
         <SafeAreaView style={{flex: 1}}>
             <Searchbar
@@ -18,11 +79,7 @@ const WaifuList = (props) => {
                 onChangeText={(e) => { setSearchQuery(e) }}
                 value={searchQuery}
             />
-            <FlatList 
-                data={props.waifus.filter(waifu => { return waifu.name.toLowerCase().includes(searchQuery.toLowerCase())})}
-                renderItem={renderWaifuCard}
-                keyExtractor={item => item._id}
-            />
+            {filterWaifuList()}
         </SafeAreaView>
     )
 }
